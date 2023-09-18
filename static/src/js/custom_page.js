cc26d3be9eec31027cbddf3192224930315f7110
Krushal Kalkani <krushal.kalkani78@gmial.com>
2023-09-18 17:07:10 +0530

    Updated Project

--- static/src/js/custom_page.js
+++ static/src/js/custom_page.js
@@ -1,14 +1,16 @@
-// Example JavaScript code (using jQuery for simplicity):
-$("#school-form").submit(function (e) {
-  e.preventDefault(); // Prevent the default form submission
-  var formData = $(this).serialize(); // Serialize the form data
+// // Example JavaScript code (using jQuery for simplicity):
+// $("#school-form").submit(function (e) {
+//   e.preventDefault(); // Prevent the default form submission
+//   var formData = $(this).serialize(); // Serialize the form data
 
-  $.ajax({
-    type: "POST",
-    url: "/custom_page", // Define the URL for form submission
-    data: formData,
-    success: function (data) {
-      alert("Data is submitted");
-    },
-  });
-});
+//   $.ajax({
+//     type: "POST",
+//     url: "/custom_page", // Define the URL for form submission
+//     data: formData,
+//     success: function (data) {
+//       alert("Data is submitted");
+//     },
+//   });
+// });
+
+hj;
